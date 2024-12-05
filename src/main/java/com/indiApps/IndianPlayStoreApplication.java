1f8619daa2c1fd1079733f01a8b0ca154a25eb12
shashank2707 <kingbreath2707@gmail.com>
2024-12-05 12:30:25 +0530

    Initial commit

---
+++ src/main/java/com/indiApps/IndianPlayStoreApplication.java
@@ -0,0 +1,13 @@
+package com.indiApps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndianPlayStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndianPlayStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indiApps/IndianPlayStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indiApps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndianPlayStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
